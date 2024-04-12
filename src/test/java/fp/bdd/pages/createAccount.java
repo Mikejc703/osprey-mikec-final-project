a10db8b0aeb5f6f02d03c43414681f82ce231c02
Mike Cunningham <michael.j.cunningham10@gmail.com>
2024-04-11 20:33:03 -0400

    my second fp push

--- src/test/java/fp/bdd/pages/createAccount.java
+++ src/test/java/fp/bdd/pages/createAccount.java
@@ -5,5 +5,4 @@ import org.openqa.selenium.By;
 public class createAccount {
 
     public static final By NEW_ACC_FORM_TITLE = By.xpath("//*[text()='Create Primary Account Holder']");
-            //h2.chakra-heading
 }
